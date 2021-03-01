57fd7e7091d71573dda40686b2e781b48cb8bb23
Vuk Antanaskovic <59714011+VukAntanaskovic@users.noreply.github.com>
2021-03-01 18:02:03 +0100

    Financial-symbols-application
    Financial-symbols-application

---
+++ FEtest/FEtest/API/IApiCallback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEtest.API
+{
+    public interface IApiCallback
+    {
+        void showLoading();
+        void hideLoading();
+
+        void onGetResult(string result);
+
+        void onErrorResult(string message);
+    }
+}
